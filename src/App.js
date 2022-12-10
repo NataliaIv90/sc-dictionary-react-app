634cb46edb2967c0803477400afd197e499fdebf
Natalia Ivantsova <ivantsovana@gmail.com>
2022-12-10 16:55:14 +0200

    added dictionary api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <Dictionary />
+      <header>
+        <h1>Dictionary React App</h1>
+      </header>
+
+      <main>
+        <Dictionary />
+      </main>
+
+      <footer>
+        <p>Open-source code by Nayalia Ivantsova</p>
+      </footer>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
   function setTheKeyword(event) {
     setKeyword(event.target.value);
   }
 
+  function handleResponce(responce) {
+    console.log(responce.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for the ${keyword}`);
+
+    // Documentaion --- https://dictionaryapi.dev/
+
+    axios.get(apiUrl).then(handleResponce);
   }
 
   return (
